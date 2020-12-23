744ed818e154c7ffda605120220fedafb5e71b5a
Jeff Bucklew <bucklew.jeff@gmail.com>
2020-12-23 08:50:56 -0500

    add test data

--- grails-app/init/test/rest/resource/controller/BootStrap.groovy
+++ grails-app/init/test/rest/resource/controller/BootStrap.groovy
@@ -3,6 +3,8 @@ package test.rest.resource.controller
 class BootStrap {
 
     def init = { servletContext ->
+        new Student(firstName: 'Jeff', lastName: 'Bucklew', age: 41).save()
+        new Student(firstName: 'Teddy', lastName: 'Skidoo', age: 23).save()
     }
     def destroy = {
     }
